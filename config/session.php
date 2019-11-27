1f59aa7c60ce4b7fa873a690ee2008dbdbad6242
Mike <mltefive@gmail.com>
2019-11-27 16:10:00 +0900

    expite_session_on_close

--- config/session.php
+++ config/session.php
@@ -33,7 +33,7 @@ return [
 
     'lifetime' => env('SESSION_LIFETIME', 120000),
 
-    'expire_on_close' => false,
+    'expire_on_close' => true,
 
     /*
     |--------------------------------------------------------------------------
