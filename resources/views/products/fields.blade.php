3cc871759d74956a8756f6ad5b563b18ab32ef31
Mike <mltefive@gmail.com>
2019-12-03 08:22:39 +0900

    fix product edit col-md

--- resources/views/products/fields.blade.php
+++ resources/views/products/fields.blade.php
@@ -1,40 +1,43 @@
 <!-- Submit Field -->
-<div class="form-group col-sm-12">
+<div class="form-group col-md-12">
     {!! Form::submit('Сохранить', ['class' => 'btn btn-primary']) !!}
     <a href="{!! route('products.index') !!}" class="btn btn-default">Отмена</a>
 </div>
 
-<!-- Ident Field -->
-<div class="form-group col-sm-2">
-    {!! Form::label('ident', 'Ident:') !!}
-    {!! Form::text('ident', null, ['class' => 'form-control']) !!}
-</div>
+{{-- <div class="row"> --}}
 
-<!-- Menu Field -->
-<div class="form-group col-sm-2">
-    {!! Form::label('menu', 'В меню:') !!}
-    <label class="checkbox-inline form-control" style="    text-align: center;">
-        {!! Form::hidden('menu', 0) !!}
-        {!! Form::checkbox('menu', '1', null) !!}
-    </label>
-</div>
+    <!-- Ident Field -->
+    <div class="form-group col-md-2">
+        {!! Form::label('ident', 'Ident:') !!}
+        {!! Form::text('ident', null, ['class' => 'form-control']) !!}
+    </div>
 
-<div class="form-group col-sm-2">
-    {!! Form::label('position', 'Порядок в списке:') !!}
-    {!! Form::text('position', null, ['class' => 'form-control']) !!}
-</div>
+    <!-- Menu Field -->
+    <div class="form-group col-md-2">
+        {!! Form::label('menu', 'В меню:') !!}
+        <label class="checkbox-inline form-control" style="    text-align: center;">
+            {!! Form::hidden('menu', 0) !!}
+            {!! Form::checkbox('menu', '1', null) !!}
+        </label>
+    </div>
+
+    <div class="form-group col-md-2">
+        {!! Form::label('position', 'Порядок:') !!}
+        {!! Form::text('position', null, ['class' => 'form-control']) !!}
+    </div>
 
 
 
-<!-- Name Field -->
-<div class="form-group col-sm-6">
-    {!! Form::label('name', 'Название:') !!}
-    {!! Form::text('name', null, ['class' => 'form-control']) !!}
-</div>
+    <!-- Name Field -->
+    <div class="form-group col-md-6">
+        {!! Form::label('name', 'Название:') !!}
+        {!! Form::text('name', null, ['class' => 'form-control']) !!}
+    </div>
 
+{{-- </div> --}}
 
 <!-- Image Field -->
-<div class="form-group col-sm-6">
+<div class="form-group col-md-6">
     {!! Form::label('image', 'Фото:') !!}
     <img class="img-responsive" src="{{ $product->image ?? 'http://placehold.it/100x70' }}">
     {{ $product->image ?? "http://placehold.it/100x70" }}
@@ -43,39 +46,39 @@
 {{-- <div class="clearfix"></div> --}}
 
 <!-- Xml Name Field -->
-<div class="form-group col-sm-6">
+<div class="form-group col-md-6">
     {!! Form::label('xml_name', 'Xml Имя:') !!}
     {!! Form::text('xml_name', null, ['class' => 'form-control']) !!}
 </div>
 
 <!-- Xml Cat Field -->
-{{-- <div class="form-group col-sm-6">
+{{-- <div class="form-group col-md-6">
     {!! Form::label('xml_cat', 'Xml Категория:') !!}
     {!! Form::text('xml_cat', null, ['class' => 'form-control']) !!}
 </div>
  --}}
 <!-- Cat Id Field -->
-<div class="form-group col-sm-6">
+<div class="form-group col-md-6">
     {!! Form::label('cat_id', 'Категория:') !!}
     {{-- {!! Form::text('cat_id', null, ['class' => 'form-control']) !!} --}}
     {!!  Form::select('cat_id', App\Models\Cat::all()->pluck('name', 'ident'), null, ['class' => 'form-control']) !!}
 </div>
 
 <!-- Price Amount Field -->
-<div class="form-group col-sm-6">
+<div class="form-group col-md-6">
     {!! Form::label('price_amount', 'Цена:') !!}
     {!! Form::text('price_amount', null, ['class' => 'form-control']) !!}
 </div>
 
 <!-- Desc Field -->
-<div class="form-group col-sm-12 col-lg-12">
+<div class="form-group col-md-12 col-lg-12">
     {!! Form::label('desc', 'Описание:') !!}
     {!! Form::textarea('desc', null, ['class' => 'form-control']) !!}
 </div>
 
 
 <!-- Submit Field -->
-<div class="form-group col-sm-12">
+<div class="form-group col-md-12">
     {!! Form::submit('Сохранить', ['class' => 'btn btn-primary']) !!}
     <a href="{!! route('products.index') !!}" class="btn btn-default">Отмена</a>
 </div>
